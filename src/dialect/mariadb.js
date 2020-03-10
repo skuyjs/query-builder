336c9523a0cfc4c571c5ab635ffa7cdc91a64ef5
Hadi Hidayat Hammurabi <hadihammurabi@gmail.com>
2020-03-10 19:14:51 +0700

    now support mongodb

---
+++ src/dialect/mariadb.js
@@ -0,0 +1,40 @@
+const mariadb = require('mariadb');
+
+const connect = (options) => mariadb.createPool({
+  host: options.host || 'localhost',
+  user: options.username || 'root',
+  database: options.database || 'test',
+  password: options.password || '',
+  waitForConnections: options.waitForConnections || true,
+  connectionLimit: options.connectionLimit || 100,
+  queueLimit: options.queueLimit || 0,
+});
+
+const select = (cols, table, where) => {
+  let wheres = '';
+
+  if (!!where) {
+    wheres = `WHERE ${where}`;
+  }
+
+  return `SELECT ${cols} FROM ${table} ${wheres}`.trim();
+};
+
+const exec = (connection, query) => new Promise((resolve, reject) => {
+  connection
+    .getConnection()
+    .then(con => {
+      con
+        .query(query)
+        .then(d => d.filter(a => d.meta))
+        .then(resolve)
+        .catch(reject)
+        .finally(con.release);
+    });
+});
+
+module.exports = {
+  connect,
+  exec,
+  select,
+};
