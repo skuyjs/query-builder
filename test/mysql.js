c7323168b3921ca1a6ce34238276595d7e819170
Hadi Hidayat Hammurabi <hadihammurabi@gmail.com>
2020-03-13 12:56:43 +0700

    mysq add insert test

--- test/mysql.js
+++ test/mysql.js
@@ -11,14 +11,14 @@ const db = new Database({
 
 db.connection.query('CREATE TABLE IF NOT EXISTS users (id int primary key auto_increment, fullname varchar(255), email varchar(255), password varchar(255))');
 
-test(`should return \`${dialect}\``, t => {
+test(`return \`${dialect}\``, t => {
   t.is(
     db.getDialect(),
     dialect
   );
 });
 
-test('should result.query is `SELECT * FROM users`', async (t) => {
+test('result.query is `SELECT * FROM users`', async (t) => {
   const result = await db.table('users').all();
   t.is(
     result.query,
@@ -26,7 +26,7 @@ test('should result.query is `SELECT * FROM users`', async (t) => {
   );
 });
 
-test('should result.query is `SELECT email, password, fullname FROM users`', async (t) => {
+test('result.query is `SELECT email, password, fullname FROM users`', async (t) => {
   const result = await db.table('users').get('email', 'password', 'fullname');
   t.is(
     result.query,
@@ -34,7 +34,7 @@ test('should result.query is `SELECT email, password, fullname FROM users`', asy
   );
 });
 
-test('should result.query is `SELECT * FROM users WHERE id=1`', async (t) => {
+test('result.query is `SELECT * FROM users WHERE id=1`', async (t) => {
   const result = await db.table('users').where({id: 1}).get();
   t.is(
     result.query,
@@ -42,15 +42,24 @@ test('should result.query is `SELECT * FROM users WHERE id=1`', async (t) => {
   );
 });
 
-// test('should result.query is `INSERT INTO users(email, password, fullname) VALUES("email", "password", "fullname")`', async (t) => {
-//   const result = await db.table('users').insert({
-//     email: 'email',
-//     password: 'password',
-//     fullname: 'fullname',
-//   });
-//
-//   t.is(
-//     result.query,
-//     'INSERT INTO users(email, password, fullname) VALUES("email", "password", "fullname")'
-//   );
-// });
+test('result.query is `INSERT INTO users(email, password, fullname) VALUES("email", "password", "fullname")`', async (t) => {
+  const result = await db.table('users').insert({
+    email: 'email',
+    password: 'password',
+    fullname: 'fullname',
+  });
+
+  t.is(
+    result.query,
+    'INSERT INTO users(email, password, fullname) VALUES("email", "password", "fullname")'
+  );
+});
+
+test('result.query is `INSERT INTO users VALUES("email", "password", "fullname")`', async (t) => {
+  const result = await db.table('users').insert(['email', 'password', 'fullname']);
+
+  t.is(
+    result.query,
+    'INSERT INTO users VALUES("email", "password", "fullname")'
+  );
+});
